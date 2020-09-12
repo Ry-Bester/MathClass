a6297a235b18f9b30d1c81b4964b87b7d03d6fd6
Ryan <zelda0169@gmailcom>
2020-09-12 15:28:32 -0600

    Add project files.

---
+++ Integers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    public class Integers
+    {
+        public int myMethod(int x)
+        {
+            int result = x + 5;
+            return result;
+        }
+        public int myMethod2(int x)
+        {
+            int result = x * 2;
+            return result;
+        }
+        public int myMethod3(int x)
+        {
+            int result = x / 3;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number to add");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Integers intobj1 = new Integers();
+            int result1 = intobj1.myMethod(num1);
+            Console.WriteLine(result1);
+            
+            Console.WriteLine("Pick a number to mulitply");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result2 = intobj1.myMethod2(num2);
+            Console.WriteLine(result2);
+
+            Console.WriteLine("Pick a number to divide");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int result3 = intobj1.myMethod3(num3);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
